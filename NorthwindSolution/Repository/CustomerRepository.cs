4ca27223369349b409ad88eaddb7bb06695601b2
arkapravasinha <arkapravasinha@ARKAWINDOWS>
2019-11-11 17:16:53 +0000

    Testing asynchronous

---
+++ NorthwindSolution/Repository/CustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿using NorthwindSolution.Repository.Interfaces;
+using NothwindDAL;
+using NothwindDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data.Entity;
+
+namespace NorthwindSolution.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        CustomerDAL customerDAL = null;
+
+        public CustomerRepository()
+        {
+            customerDAL = new CustomerDAL();
+        }
+        public Task<List<Customer>> GetCustomersAsync()
+        {
+            return Task.FromResult<List<Customer>>(customerDAL.GetCustomers());         
+        }
+    }
+}
---
+++ NorthwindSolution/Repository/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using NothwindDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindSolution.Repository.Interfaces
+{
+    interface ICustomerRepository
+    {
+        Task<List<Customer>> GetCustomersAsync();
+    }
+}
